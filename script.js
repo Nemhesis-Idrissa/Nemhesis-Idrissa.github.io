51e04b2eb689adfeb83708aa8cba12c236536897
Nemhesis-Idrissa <43876545+Nemhesis-Idrissa@users.noreply.github.com>
2024-11-08 22:25:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.my-button').addEventListener('click', function() {
+        location.href = 'https://www.univ-paris8.fr/-Master-Cyber-Securite-et-Sciences-des-Donnees-';
+    });
+});
